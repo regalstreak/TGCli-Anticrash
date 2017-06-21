4dbdcc98ce8bd4f71258e58b462a4aafbce8f61c
Neil Agarwal <regalstreak@gmail.com>
2017-06-21 22:10:51 +0000

    Add simpler config

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
+bots = {
+    "XManager": [4488, "/home/regalstreak2/bot/xmanager", "regalstreak2"]
+    }
 check_time = 15
+
+
+"""
+Example:
+bots = {
+    "Name Of Bot": [Some Port (Doesn't matter which use default), "Bot path containing launch.sh", "User with which bot is run"]
+    }
+check_time = 15
+"""
